1b1dc4858f70a38db21fc61fb84e3d9acb469756
not6248 <96365700+not6248@users.noreply.github.com>
2025-12-17 21:08:13 +0700

    last

---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const url = config.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: true,
+// })
+
+// // note.save().then((result) => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
